06f340b0daccf8b26975eca4f4b0b73a5a49d989
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-08-21 17:20:05 +0200

    Add DP implementation and tests.

--- dynamic_programming/count_palindromic_substrings.py
+++ dynamic_programming/count_palindromic_substrings.py
@@ -7,7 +7,29 @@ def count_palindromic_substrings_no_dp(string: str):
     n = len(string)
     count = 0
     for i in range(0, n):
-        for j in range(i+1, n+1):
+        for j in range(i + 1, n + 1):
             if string[i:j] == string[i:j][::-1]:
                 count += 1
+    return count
+
+
+def count_palindromic_substrings(string: str):
+    n = len(string)
+    count = 0
+
+    for palindrome_center in range(0, n):
+        # Count odd length palindromes
+        left = palindrome_center
+        right = palindrome_center
+        while left > -1 and right < n and string[left] == string[right]:
+            left -= 1
+            right += 1
+            count += 1
+        # Count even length palindromes
+        left = palindrome_center
+        right = palindrome_center + 1
+        while left > -1 and right < n and string[left] == string[right]:
+            left -= 1
+            right += 1
+            count += 1
     return count
--- tests/test_dynamic_programming/test_count_palindromic_substrings.py
+++ tests/test_dynamic_programming/test_count_palindromic_substrings.py
@@ -1,4 +1,4 @@
-from dynamic_programming.count_palindromic_substrings import count_palindromic_substrings_no_dp
+from dynamic_programming.count_palindromic_substrings import count_palindromic_substrings_no_dp, count_palindromic_substrings
 
 
 def test_count_palindromic_substrings_no_dp():
@@ -23,3 +23,67 @@ def test_count_palindromic_substrings_no_dp4():
     string = "dnncbwoneinoplypwgbwktmvkoimcooyiwirgbxlcttgteqthcvyoueyftiwgwwxvxvg"
     expected_result = 77
     assert expected_result == count_palindromic_substrings_no_dp(string)
+
+
+def test_count_palindromic_substrings_no_dp5():
+    string = "longtimenosee"
+    expected_result = 14
+    assert expected_result == count_palindromic_substrings_no_dp(string)
+
+
+def test_count_palindromic_substrings_no_dp6():
+    string = "cuuedxumdolqdytmcudgqmxcwelatcphpvmqqgahbgjklekehgsulsyuhdxaggumsqpktltsytkoo"
+    expected_result = 84
+    assert expected_result == count_palindromic_substrings_no_dp(string)
+
+
+def test_count_palindromic_substrings_np_dp7():
+    string = "ttttttttttttttttttttt"
+    expected_result = 231
+    assert expected_result == count_palindromic_substrings_no_dp(string)
+
+##################################################################################################################
+##################################################################################################################
+##################################################################################################################
+
+
+def test_count_palindromic_substrings():
+    string = "abc"
+    expected_result = 3
+    assert expected_result == count_palindromic_substrings(string)
+
+
+def test_count_palindromic_substrings2():
+    string = "aaa"
+    expected_result = 6
+    assert expected_result == count_palindromic_substrings(string)
+
+
+def test_count_palindromic_substrings3():
+    string = "abba"
+    expected_result = 6
+    assert expected_result == count_palindromic_substrings(string)
+
+
+def test_count_palindromic_substrings4():
+    string = "dnncbwoneinoplypwgbwktmvkoimcooyiwirgbxlcttgteqthcvyoueyftiwgwwxvxvg"
+    expected_result = 77
+    assert expected_result == count_palindromic_substrings(string)
+
+
+def test_count_palindromic_substrings5():
+    string = "longtimenosee"
+    expected_result = 14
+    assert expected_result == count_palindromic_substrings(string)
+
+
+def test_count_palindromic_substrings6():
+    string = "cuuedxumdolqdytmcudgqmxcwelatcphpvmqqgahbgjklekehgsulsyuhdxaggumsqpktltsytkoo"
+    expected_result = 84
+    assert expected_result == count_palindromic_substrings(string)
+
+
+def test_count_palindromic_substrings7():
+    string = "ttttttttttttttttttttt"
+    expected_result = 231
+    assert expected_result == count_palindromic_substrings(string)
