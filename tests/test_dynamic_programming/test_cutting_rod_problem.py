03c82860e02f02e0524d8f08c54c178b480ba932
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-09-01 16:29:54 +0200

    Add tests for memoization implementation.

--- tests/test_dynamic_programming/test_cutting_rod_problem.py
+++ tests/test_dynamic_programming/test_cutting_rod_problem.py
@@ -24,3 +24,25 @@ def test_cut_rod_recursive3():
 #############################################################################################################
 #############################################################################################################
 #############################################################################################################
+
+
+def test_cut_rod_top_down1():
+    prices = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    length = 8
+    expected_result = 22
+    assert expected_result == cut_rod_top_down(length, prices)
+
+
+def test_cut_rod_top_down2():
+    prices = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    length = 9
+    expected_result = 25
+    assert expected_result == cut_rod_top_down(length, prices)
+
+
+def test_cut_rod_top_down3():
+    prices = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    length = 15
+    expected_result = 43
+    assert expected_result == cut_rod_top_down(length, prices)
+
