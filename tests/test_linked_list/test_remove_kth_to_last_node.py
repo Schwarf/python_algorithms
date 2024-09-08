0054d0f6da3ff2008bba07d5065cde179673a16e
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-09-08 10:52:30 +0200

    Start refactoring linked list tests.

--- tests/test_linked_list/test_remove_kth_to_last_node.py
+++ tests/test_linked_list/test_remove_kth_to_last_node.py
@@ -1,49 +1,15 @@
 from linked_lists.remove_kth_to_last_node import remove_kth_to_last_node
 from tests.test_linked_list.helper_functions import build_linked_list, list_to_values
+import pytest
+
+test_cases = [([1, 2, 3, 4, 5], 2, [1, 2, 3, 5]),
+              ([1, 2, 3, 4, 5], 1, [1, 2, 3, 4]),
+              ([1, 2, 3], 3, [2, 3]),
+              ([1, 2], 5, [1, 2])]
 
 
-def test_remove_2nd_to_last_node():
-    # Create a linked list from a list of values
-    values = [1, 2, 3, 4, 5]
-    head = build_linked_list(values)
-
-    # Remove the 2nd to last node
-    modified_head = remove_kth_to_last_node(head, 2)
-
-    # Check the result
-    assert list_to_values(modified_head) == [1, 2, 3, 5]
-
-def test_remove_last_node():
-    # Create a linked list from a list of values
-    values = [1, 2, 3, 4, 5]
-    head = build_linked_list(values)
-
-    # Remove the 2nd to last node
-    modified_head = remove_kth_to_last_node(head, 1)
-
-    # Check the result
-    assert list_to_values(modified_head) == [1, 2, 3, 4]
-
-
-def test_remove_head_node():
-    values = [1, 2, 3]
-    head = build_linked_list(values)
-
-    # Remove the head node (3rd to last)
-    modified_head = remove_kth_to_last_node(head, 3)
-
-    # Check the result
-    assert list_to_values(modified_head) == [2, 3]
-
-
-def test_remove_beyond_length():
-    values = [1, 2]
-    head = build_linked_list(values)
-
-    # Attempt to remove a node beyond the length of the list
-    modified_head = remove_kth_to_last_node(head, 5)
-
-    # Expect no changes as the position is invalid
-    assert list_to_values(modified_head) == [1, 2]
-
-# Additional tests can be written to cover more scenarios
+@pytest.mark.parametrize("list_input, k, expected_result", test_cases)
+def test_remove_2nd_to_last_node(list_input, k, expected_result):
+    head = build_linked_list(list_input)
+    modified_head = remove_kth_to_last_node(head, k)
+    assert list_to_values(modified_head) == expected_result
--- tests/test_linked_list/test_split_linked_list_in_parts.py
+++ tests/test_linked_list/test_split_linked_list_in_parts.py
@@ -1,8 +1,7 @@
 from typing import List
 
 from linked_lists.split_linked_list_in_parts import split_linked_list_in_parts
-from linked_lists.node import Node
-
+from tests.test_linked_list.helper_functions import build_linked_list, list_to_values
 import pytest
 
 test_cases = [([1, 2, 3], 5, [[1], [2], [3], [], []]),
@@ -11,27 +10,8 @@ test_cases = [([1, 2, 3], 5, [[1], [2], [3], [], []]),
               ([1], 1, [[1]])]
 
 
-def build_linked_list(values):
-    if not values:
-        return None
-    head = Node(values[0])
-    current = head
-    for value in values[1:]:
-        current.next = Node(value)
-        current = current.next
-    return head
-
-
-def linked_list_to_list(head):
-    result = []
-    while head:
-        result.append(head.data)
-        head = head.next
-    return result
-
-
 @pytest.mark.parametrize("list_input, k, expected_result", test_cases)
 def test_split_linked_list_in_parts(list_input: List[int], k: int, expected_result: List[List[int]]):
     linked_list = build_linked_list(list_input)
     result = split_linked_list_in_parts(linked_list, k)
-    assert [linked_list_to_list(part) for part in result] == expected_result
+    assert [list_to_values(part) for part in result] == expected_result
